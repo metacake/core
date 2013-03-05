4c6a0f5b8b31ffa67fbbe622cbfd895e5079b3d9
Spencer Florence <florence@ccs.neu.edu>
2013-03-05 15:07:28 -0500

    setup pom

---
+++ src/test/java/com/cake/TestTemp.java
@@ -0,0 +1,25 @@
+package com.cake;
+
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: spencerflorence
+ * Date: 3/5/13
+ * Time: 2:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestTemp  {
+
+    @Test
+    public void testTemp() {
+        assertEquals((new Temp()).test, 1);
+    }
+
+}
+
+
+
+
+
