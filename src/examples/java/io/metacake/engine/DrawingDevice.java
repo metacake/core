7f954529fc71f904d258e1c6798105cba86189b8
Spencer Florence <florence@ccs.neu.edu>
2013-03-17 18:40:20 -0400

    trying to find source of lag

--- src/examples/java/io/metacake/engine/DrawingDevice.java
+++ src/examples/java/io/metacake/engine/DrawingDevice.java
@@ -1,5 +1,6 @@
 package io.metacake.engine;
 
+import io.metacake.core.common.MilliTimer;
 import io.metacake.core.common.window.CakeWindow;
 import io.metacake.core.common.window.CloseObserver;
 import io.metacake.core.output.OutputDevice;
@@ -39,21 +40,28 @@ public class DrawingDevice implements OutputDevice {
         draw = window.draw;
     }
 
+    private MilliTimer t = new MilliTimer();
+
     void draw(List<RenderingInstruction> r) {
+        System.out.print("starting draw: " + t.update() + "\t");
         Graphics g = draw.bufferStrategy.getDrawGraphics();
         g.clearRect(0, 0, draw.getWidth(), draw.getHeight());
+        System.out.print(t.update() + "\t");
         for (RenderingInstruction d : r) {
             ((DrawInstruction) d).draw(g);
         }
+        System.out.print(t.update() + "\t");
         g.dispose();
+        System.out.print(t.update() + "\t");
         draw.bufferStrategy.show();
+        System.out.println(t.update());
     }
 }
 
 // TODO: generic no shitty thread. FUCK YOU ORACLE
 class DrawingThread extends Thread {
-    DrawingDevice d;
-    volatile boolean running = false;
+    volatile DrawingDevice d;
+    volatile boolean running = true;
 
     DrawingThread(DrawingDevice d) {
         this.d = d;
@@ -61,11 +69,13 @@ class DrawingThread extends Thread {
 
     @Override
     public void run() {
+        MilliTimer t = new MilliTimer();
         for (;running; ) {
             try {
                 d.draw(d.queue.take());
             } catch (InterruptedException e) {
                 this.running = false;
+                e.printStackTrace();
             }
         }
     }
