3467747431b1542a21211dbe3b09e9bbc01f278a
Spencer Florence <florence@ccs.neu.edu>
2013-11-11 20:00:58 -0500

    finished off CustomMapTest->CustomMapSpec

--- src/test/groovy/io/metacake/core/common/CustomMapSpec.groovy
+++ src/test/groovy/io/metacake/core/common/CustomMapSpec.groovy
@@ -4,33 +4,42 @@ import spock.lang.Specification
 import java.util.concurrent.Callable
 
 import static org.hamcrest.CoreMatchers.equalTo
+import static org.hamcrest.CoreMatchers.notNullValue
+import static org.spockframework.util.Assert.notNull
+import static org.spockframework.util.Matchers.not
 import static spock.util.matcher.HamcrestSupport.expect
 
 class CustomMapSpec extends Specification {
 
-    CustomizableMap<Integer, String> map = new CustomizableMap([:])
+    CustomizableMap<Symbol, String> map = new CustomizableMap([:])
 
     def "Optional Default returns the specified value"() {
         setup: Callable res = {'irrelevant'}
-        expect: expect map.get(0, res), equalTo('irrelevant')
+        expect: expect map.get(Symbol.genSym(), res), equalTo('irrelevant')
     }
 
     def "Optional Default returns the specified value when set"() {
         setup: map.setOnMissing {'irrelevant'}
-        expect: map.get(0) == 'irrelevant'
+        expect: map.get(Symbol.genSym()) == 'irrelevant'
     }
 
     def "When no default is provided the get throws an error on a missing element"() {
-        when: map.get(0)
+        when: map.get(Symbol.genSym())
         then: thrown RuntimeException
     }
 
     def "Can still get an existing key"() {
-        setup: map.put 0, 'data'
-        expect: map.get(0) == 'data'
+        setup:
+        def name = Symbol.genSym()
+        map.put name, 'data'
+        expect: map.get(name) == 'data'
     }
 
     def "Does it iterate over the entry set"() {
-
+        setup:
+        def elements = (0..10).collect {Symbol.genSym(it.toString())}
+        elements.each {map.put(it,it.toString())}
+        expect:
+        map.iterator().toList() == map.entrySet().toList()
     }
 }
