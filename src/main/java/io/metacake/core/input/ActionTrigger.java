9587940caea3b8b26d730a0edc17125380ee1f7b
Ryan Plessner <plessner.ryan@gmail.com>
2013-12-21 09:02:22 -0500

    changed the actiontrigger to be parameterized over hte type of event it is triggered by.

--- src/main/java/io/metacake/core/input/ActionTrigger.java
+++ src/main/java/io/metacake/core/input/ActionTrigger.java
@@ -7,13 +7,17 @@ import java.util.List;
  *
  * @author florence
  * @author rpless
+ *
+ * @param <K> The type of the "low-level" object that is passed to this trigger by the {@link io.metacake.core.input.system.InputDevice}
+ *            it is bound to.
  */
 public interface ActionTrigger<K> {
 
     /**
-     * @return Get the codes that this ActionTrigger will respond to.
+     * @param event The "low-level" event that is being checked.
+     * @return Returns true if the event triggers this {@code ActionTrigger}.
      */
-    public List<K> getCodes();
+    public boolean isTriggeredBy(K event);
 
     /**
      * @return Get the name of the InputDevice that this ActionTrigger is bound to.
